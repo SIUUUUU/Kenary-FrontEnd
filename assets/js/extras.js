d0080da0d2926fe3520db4784f85b3a728858a67
dagr27 <00118717@uca.edu.sv>
2019-12-04 17:49:41 -0600

    Subiendo Casi final

--- assets/js/extras.js
+++ assets/js/extras.js
@@ -2,6 +2,7 @@ document.getElementById("logOut").addEventListener("click",()=>{
     window.localStorage.setItem("student",  null);
     window.localStorage.setItem("admin",  null);
     window.localStorage.setItem("tutor",  null);
+    window.localStorage.setItem("idUpdate", null);
     session = null;
     window.location= "../../dist/login.html";
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,32 @@
-function login(){
+function login() {
 	username = document.getElementById("username").value;
 	pass = document.getElementById("password").value;
-	axios.get('http://localhost:8080/login/'+username+'/'+pass)
-	.then(function(response){
-		if(response.data["uPassword"] == pass){
-			if(response.data["uType"] == "Estudiante"){
-				window.localStorage.setItem("student",  username);
-				window.location = "../modules/student/home.html";
-			}else if(response.data["uType"] == "Tutor"){
-				window.localStorage.setItem("tutor",  username);
-				window.location = "../modules/tutor/home.html";
-			}else{
-				window.localStorage.setItem("admin",  username);
-				window.location = "../modules/admin/home.html";
+	axios.get('http://localhost:8080/login/' + username + '/' + pass)
+		.then(function (response) {
+			if (response.data["uPassword"] == pass) {
+				if (response.data["uType"] == "Estudiante") {
+					window.localStorage.setItem("student", response.data["cUser"]);
+					window.location = "../modules/student/home.html";
+				} else if (response.data["uType"] == "Tutor") {
+					window.localStorage.setItem("tutor", response.data["cUser"]);
+					window.location = "../modules/tutor/home.html";
+				} else {
+					window.localStorage.setItem("admin", response.data["cUser"]);
+					window.location = "../modules/admin/home.html";
+				}
+			} else {
+				alert("Datos de inicio de Sesion Erroneos");
 			}
-		}else{
-			alert("Datos de inicio de Sesion Erroneos");
-		}
-	})
-	.catch(function(error){
-		console.log(error)
-	})
-	.then(function (){
+		})
+		.catch(function (error) {
+			console.log(error)
+		})
+		.then(function () {
 
-	});
+		});
 }
 
-function register(){
+function register() {
 	name = document.getElementById("name").value;
 	lastname = document.getElementById("lastname").value;
 	username = document.getElementById("username").value;
@@ -38,36 +38,39 @@ function register(){
 	grade = document.getElementById("grade").value;
 	type = document.getElementById("type").value;
 	career = document.getElementById("career").value;
-	if(pass == repass){
-		axios.post('http://localhost:8080/user',{
-		"uPhoto": "mifoto.png",
-		"uFirstName": name,
-		"uLastName": lastname,
-		"uUsername": username,
-		"uPassword": pass,
-		"uBirthday": date,
-		"uCountry": "El Salvador",
-		"uInstitution": "UCA",
-		"uGender": gender,
-		"uDegree": grade,
-		"uType": type,
-		"uCareer": career
-	})
-	.then(function(response){
-		consolse.log(response);
-	})
-	.catch(function(error){
-		console.log(error);
-	})
-	.then(function(){
-		window.location = "login.html";
-	});
-	}else{
+	if (pass == repass) {
+		axios.post('http://localhost:8080/user', {
+			"uPhoto": "mifoto.png",
+			"uFirstName": name,
+			"uLastName": lastname,
+			"uUsername": username,
+			"uPassword": pass,
+			"uBirthday": date,
+			"uCountry": "El Salvador",
+			"uInstitution": "UCA",
+			"uGender": gender,
+			"uDegree": grade,
+			"uType": type,
+			"uCareer": career
+		})
+			.then(function (response) {
+				consolse.log(response);
+			})
+			.catch(function (error) {
+				console.log(error);
+			})
+			.then(function () {
+				window.location = "login.html";
+			});
+	} else {
 		alert("Las contraseñas no Coinciden");
 	}
 }
 
-function updateUser(){
+function updateUser() {
+	const headers = {
+		"Content-Type": "application/json"
+	  };
 	id = document.getElementById("id").value;
 	name = document.getElementById("name").value;
 	lastname = document.getElementById("lastname").value;
@@ -80,178 +83,181 @@ function updateUser(){
 	grade = document.getElementById("grade").value;
 	type = document.getElementById("type").value;
 	career = document.getElementById("career").value;
-	if(pass == repass){
-		axios.put('http://localhost:8080/user/'+id,{
-		"uPhoto": "mifoto.png",
-		"uFirstName": name,
-		"uLastName": lastname,
-		"uUsername": username,
-		"uPassword": pass,
-		"uBirthday": date,
-		"uCountry": "El Salvador",
-		"uInstitution": "UCA",
-		"uGender": gender,
-		"uDegree": grade,
-		"uType": type,
-		"uCareer": career
-	})
-	.then(function(response){
-		consolse.log(response);
-	})
-	.catch(function(error){
-		console.log(error);
-	})
-	.then(function(){
-		window.location = "login.html";
-	});
-	}else{
+	if (pass == repass) {
+		axios.put('http://localhost:8080/user', {
+			"cUser": parseInt(id),
+			"uPhoto": "mifoto.png",
+			"uFirstName": name,
+			"uLastName": lastname,
+			"uUsername": username,
+			"uPassword": pass,
+			"uBirthday": date,
+			"uCountry": "El Salvador",
+			"uInstitution": "UCA",
+			"uGender": gender,
+			"uDegree": grade,
+			"uType": type,
+			"uCareer": career
+		})
+			.then(function (response) {
+				consolse.log(response);
+			})
+			.catch(function (error) {
+				alert(error);
+			})
+			.then(function () {
+
+			});
+	} else {
 		alert("Las contraseñas no Coinciden");
 	}
 }
 
-function getAllUsers(){
+function getAllUsers() {
 	var div = document.getElementById('userTable');
 	var table = document.createElement('table');
 	var tableBody = document.createElement('tbody');
-  	table.appendChild(tableBody);
+	table.appendChild(tableBody);
 	axios.get('http://localhost:8080/usersList')
-	.then(function(response){
-		console.log(response.data[0]["uFirstName"]);
-		for(i = 0 ; i< response.data.length; i++){
-			console.log(i);
-			var tr = document.createElement('tr');
-			tableBody.appendChild(tr);
-			for(key in response.data[i]){
+		.then(function (response) {
+			for (i = 0; i < response.data.length; i++) {
+				console.log(i);
+				var tr = document.createElement('tr');
+				var id = response.data[i]["cUser"];
+				tableBody.appendChild(tr);
+				for (key in response.data[i]) {
+					var td = document.createElement('td');
+					td.appendChild(document.createTextNode(response.data[i][key]));
+					tr.appendChild(td);
+				}
 				var td = document.createElement('td');
-				td.appendChild(document.createTextNode(response.data[i][key]));
+				var a = document.createElement('a');
+				a.appendChild(document.createTextNode("Editar"));
+				a.href = "../profileAdmin.html?idS=" + id;
+				td.appendChild(a);
+
 				tr.appendChild(td);
 			}
 		}
-	})
-	.catch(function(error){
-		console.log(error);
-	})
-	.then(function(){
-		
-	});
+
+		)
+		.catch(function (error) {
+			console.log(error);
+		})
+		.then(function () {
+
+		});
 	div.appendChild(table);
 
 }
-function validateDuplicate(username){
-	axios.get('http://localhost:8080/login/'+username+'/123')
-	  .then(function (response) {
-		  if(response.data[""]){
+function validateDuplicate(username) {
+	axios.get('http://localhost:8080/login/' + username + '/123')
+		.then(function (response) {
+			if (response.data[""]) {
 
-		  }
-	  })
-	  .catch(function (error) {
-		console.log(error);
-	  })
-	  .then(function () {
-	  });
+			}
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
+		.then(function () {
+		});
 }
 
-function getAllRequest()
-{
+function getAllRequest() {
 	axios.get('http://localhost:8080/item')
-	  .then(function (response) {
-		console.log(response);
-	  })
-	  .catch(function (error) {
-		console.log(error);
-	  })
-	  .then(function () {
-	  });
+		.then(function (response) {
+			console.log(response);
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
+		.then(function () {
+		});
 }
 
-function getFilteredRequest()
-{
+function getFilteredRequest() {
 	axios.get('http://localhost:8080/item', {
 		params: {
-			filter : 'myFilter'
+			filter: 'myFilter'
 		}
-	  })
-	  .then(function (response) {
-		console.log(response);
-	  })
-	  .catch(function (error) {
-		console.log(error);
-	  })
-	  .then(function () {
-	  });
+	})
+		.then(function (response) {
+			console.log(response);
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
+		.then(function () {
+		});
 }
 
-function getByIdRequest()
-{
+function getByIdRequest() {
 	id = 10;
 	axios.get('http://localhost:8080/user/' + id)
-	  .then(function (response) {
-		console.log(response);
-	  })
-	  .catch(function (error) {
-		console.log(error);
-	  })
-	  .then(function () {
-	  });
+		.then(function (response) {
+			console.log(response);
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
+		.then(function () {
+		});
 }
 
-function postRequest()
-{
+function postRequest() {
 	axios.post('http://localhost:8080/item', {
 		data: 'NewItem'
-	  })
-	  .then(function (response) {
-		console.log(response);
-	  })
-	  .catch(function (error) {
-		console.log(error);
-	  })
-	  .then(function () {
-	  });
+	})
+		.then(function (response) {
+			console.log(response);
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
+		.then(function () {
+		});
 }
 
-function putRequest()
-{
+function putRequest() {
 	id = 10;
 	axios.put('http://localhost:8080/item/' + id, {
 		data: 'NewItem'
-	  })
-	  .then(function (response) {
-		console.log(response);
-	  })
-	  .catch(function (error) {
-		console.log(error);
-	  })
-	  .then(function () {
-	  });
+	})
+		.then(function (response) {
+			console.log(response);
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
+		.then(function () {
+		});
 }
 
-function patchRequest()
-{
+function patchRequest() {
 	id = 10;
 	axios.patch('http://localhost:8080/item/' + id, {
 		data: 'NewItem'
-	  })
-	  .then(function (response) {
-		console.log(response);
-	  })
-	  .catch(function (error) {
-		console.log(error);
-	  })
-	  .then(function () {
-	  });
+	})
+		.then(function (response) {
+			console.log(response);
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
+		.then(function () {
+		});
 }
 
-function deleteRequest()
-{
+function deleteRequest() {
 	id = 10;
 	axios.delete('http://localhost:8080/item/' + id)
-	  .then(function (response) {
-		console.log(response);
-	  })
-	  .catch(function (error) {
-		console.log(error);
-	  })
-	  .then(function () {
-	  });
+		.then(function (response) {
+			console.log(response);
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
+		.then(function () {
+		});
 }
